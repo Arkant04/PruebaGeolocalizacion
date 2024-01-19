1860644f33a56fe1bf5efc32933eb6c9cdee061f
Alejandro <delacorte19@gmail.com>
2024-01-19 15:38:43 +0100

    MOD: update del repo

--- main.js
+++ main.js
@@ -1,6 +1,37 @@
-navigator.geolocation.getCurrentPosition(exito, error)
+const opciones = {
+    enableHighAccuracy: true, // la llamada será más lenta pero precisa
+    timeout: 5000, // cuanto tiempo pasará hasta que se cancele la funcion (5s)
+    maximumAge: 0 //cuanto tiempo a pasao desde la ultima vez que se metio 0= cada vez que entre manda una llamada
+};
+ function crearMapa(lat, lon){
 
-function exito(pos){
-    console.log(pos)
+    var map = L.map('map', {zoomControl: false}).setView([lat, lon], 13);
+     
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+     }).addTo(map);
+     
+    L.marker([lat, lon]).addTo(map)
+    .bindPopup('Cagaste')
+    .openPopup();
+    var circle = L.circle([lat, lon], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 200
+    }).addTo(map);
+ }
+
+function exito(posicion) {
+    console.log (posicion)
+   
+    const {latitude, longitude, accuracy} = posicion.coords;
+    const {timeStamp} = posicion;
+
+    crearMapa(latitude, longitude);
 }
 
+function error(error) {
+    console.log(error); 
+}
+navigator.geolocation.getCurrentPosition(exito, error, opciones);
